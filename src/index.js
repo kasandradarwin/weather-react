93f9da5bb9862499f9e602c781d69e9e441bb58b
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-11-28 13:05:45 -0800

    trying to fix rendering issue

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Weather from "./Weather";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
     <Weather />
   </React.StrictMode>
 );
